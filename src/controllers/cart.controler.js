0f0bccdd956e8713c3216bbae00aea19da441ee0
javiscripto <javier.mecker94@gmail.com>
2023-11-16 22:40:03 -0300

    renombro cartService en cart.controler

--- src/controllers/cart.controler.js
+++ src/controllers/cart.controler.js
@@ -1,28 +1,28 @@
 import CartsMongo from "../DAO/classes/cartsClass.js";
 
-const cartMongo = new CartsMongo()
+const cartService = new CartsMongo()
 
 const createCart=async(req, res)=>{
-    const newCart= await cartMongo.createCart();
+    const newCart= await cartService.createCart();
     res.json({result:"success", payload:newCart})
 }
 
 
 const getAll=async(req,res)=>{
-    const carts= await cartMongo.getAll();
+    const carts= await cartService.getAll();
     res.json(carts)
 }
 
 const getById=async(req, res)=>{
     const cid = req.params.cid
-    const cart = await cartMongo.getById(cid);
+    const cart = await cartService.getById(cid);
     res.json(cart)
 }
 
 const addProduct= async(req, res)=>{
     const cid= req.params.cid;
     const pid= req.params.pid;
-    const result= await cartMongo.addProduct(cid,pid);
+    const result= await cartService.addProduct(cid,pid);
     res.json(result)
 }
 
@@ -30,7 +30,7 @@ const deleteProduct= async (req, res)=>{
     const cid= req.params.cid;
     const pid= req.params.pid;
 
-    const result= cartMongo.deleteProduct(cid,pid);
+    const result= cartService.deleteProduct(cid,pid);
     res.json(result)
 }
 
